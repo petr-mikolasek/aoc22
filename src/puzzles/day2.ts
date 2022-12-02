6fd053103961f6497b178e943772bab9fb1ed035
Petr Mikolasek <ghrkff@gmail.com>
2022-12-02 06:46:31 +0100

    Day 2 part 2

---
+++ src/puzzles/day2.ts
@@ -0,0 +1,61 @@
+import {intoArrays, readFileIntoString} from '../input';
+
+const rawInput: string = readFileIntoString('src/inputs/day2.txt');
+const strategyGuide: string[][] = intoArrays(rawInput.split('\r\n'), ' ');
+
+const partOne = (): number => {
+    let score = 0;
+    strategyGuide.forEach((game) => {
+        if (game[0] === 'A') {
+            if (game[1] === 'X') score += 4;
+            else if (game[1] === 'Y') score += 8;
+            else if (game[1] === 'Z') score += 3;
+            else console.error('Wut?');
+        } else if (game[0] === 'B') {
+            if (game[1] === 'X') score += 1;
+            else if (game[1] === 'Y') score += 5;
+            else if (game[1] === 'Z') score += 9;
+            else console.error('Wut?');
+        } else if (game[0] === 'C') {
+            if (game[1] === 'X') score += 7;
+            else if (game[1] === 'Y') score += 2;
+            else if (game[1] === 'Z') score += 6;
+            else console.error('Wut?');
+        } else {
+            console.error('Wut?');
+        }
+    });
+    return score;
+};
+
+const partTwo = (): number => {
+    let score = 0;
+    strategyGuide.forEach((game) => {
+        if (game[0] === 'A') {
+            if (game[1] === 'X') score += 3;
+            else if (game[1] === 'Y') score += 4;
+            else if (game[1] === 'Z') score += 8;
+            else console.error('Wut?');
+        } else if (game[0] === 'B') {
+            if (game[1] === 'X') score += 1;
+            else if (game[1] === 'Y') score += 5;
+            else if (game[1] === 'Z') score += 9;
+            else console.error('Wut?');
+        } else if (game[0] === 'C') {
+            if (game[1] === 'X') score += 2;
+            else if (game[1] === 'Y') score += 6;
+            else if (game[1] === 'Z') score += 7;
+            else console.error('Wut?');
+        } else {
+            console.error('Wut?');
+        }
+    });
+    return score;
+};
+
+console.time('doSomething');
+
+console.log(partOne());
+console.log(partTwo());
+
+console.timeEnd('doSomething');
